e2786370f52d1bf6722d760e52a2fe8ada6696b9
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-08-15 12:56:51 +0700

    app navigation

---
+++ app/src/main/java/com/example/dlearning/presentation/navigation/AppNavHost.kt
@@ -0,0 +1,44 @@
+package com.example.dlearning.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.dlearning.presentation.ui.auth.ForgetPasswordScreen
+import com.example.dlearning.presentation.ui.auth.LoginScreen
+import com.example.dlearning.presentation.ui.auth.SignupScreen
+import com.example.dlearning.presentation.ui.home.HomeScreen
+import com.example.dlearning.presentation.ui.splash.SplashScreen
+import com.example.dlearning.utils.ui.*
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUTE_SPLASH
+) {
+
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(ROUTE_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+        composable(ROUTE_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+        composable(ROUTE_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+        composable(ROUTE_HOME) {
+            HomeScreen(navController = navController)
+        }
+        composable(ROUTE_FORGET_PW) {
+            ForgetPasswordScreen(navController = navController)
+        }
+    }
+}
